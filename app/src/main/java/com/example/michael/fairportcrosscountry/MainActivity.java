9fc866cc60f88b7a3de959f3f4ebcd8387f3bcdc
Mike <herewegolakers@gmail.com>
2017-02-04 22:50:02 -0500

    roster functionality added

--- app/src/main/java/com/example/michael/fairportcrosscountry/MainActivity.java
+++ app/src/main/java/com/example/michael/fairportcrosscountry/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.michael.fairportcrosscountry;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -23,15 +24,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.hide();
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -81,22 +73,29 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.roster) {
+            rosterCardManager();
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
+        } else{}
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void rosterCardManager(View view){
+        Intent intent = new Intent(MainActivity.this, Roster.class);
+        startActivity(intent);
+    }
+
+    public void rosterCardManager(){
+        Intent intent = new Intent(MainActivity.this, Roster.class);
+        startActivity(intent);
+    }
+
 }
